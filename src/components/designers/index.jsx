19da490414c2e08042f9889b6fe7ada22f310907
Ryan O’Boyle <durbanpoison@protonmail.com>
2022-08-02 11:41:20 -0700

    updates readme

---
+++ src/components/designers/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Designers() {
+  return (
+    <div>
+      <h3>Black Lotus Designers:</h3>
+			<ul>
+				<li>DurbanPoisonPew</li>
+				<li>ModeratorGage</li>
+				<li>NeoAeon</li>
+				<li>Stark80</li>
+				<li>TheArmourer</li>
+			</ul>
+    </div>
+  );
+}
+
+export default Designers;
